bb192db24300051aafcb2fdb40eef0bca4acefbf
Harsha Yalavarthy <harsha.hy@gmail.com>
2018-04-27 11:15:58 -0500

    Intial commit for our first spring boot project

---
+++ src/main/java/com/proitdevops/ProitdevopsApplication.java
@@ -0,0 +1,12 @@
+package com.proitdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProitdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProitdevopsApplication.class, args);
+	}
+}

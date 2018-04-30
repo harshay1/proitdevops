90d541e342e8911f056226ed9fabb4b45aac0f01
Harsha Yalavarthy <harsha.hy@gmail.com>
2018-04-30 12:12:45 -0500

    added bootstrap and jquery dependencies

--- src/main/java/com/proitdevops/ProitdevopsApplication.java
+++ src/main/java/com/proitdevops/ProitdevopsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProitdevopsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProitdevopsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProitdevopsApplication.class, args);
+    }
 }
